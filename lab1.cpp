5520bbcd243ef9d632289ec334a9275c292df05c
unknown <i.ganyk.o@gmail.com>
2022-11-28 11:46:20 +0200

    adding my first lab

---
+++ lab1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+/*my first project*/
+int main() {
+	double x, y, res;
+
+	x = 2.32424
+		y = 23.4124
+
+		res = x * pow(x, 2) + y * pow(y, 2);
+	printf("res = %f\d", res);
+	return 0;
+}
